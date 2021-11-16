5aa52e3fbeaab51fb4cb2fbb2e35bcd5ececf3c7
Rashmiii1001 <63809222+Rashmiii1001@users.noreply.github.com>
2021-11-16 19:43:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ def webhook():
     req = request.get_json(silent=True, force=True)
 
     #print("Request:")
+    print("INSIDE WEBHOOK")
     #print(json.dumps(req, indent=4))
 
     res = processRequest(req)
@@ -39,6 +40,7 @@ def webhook():
 # processing the request from dialogflow
 def processRequest(req):
     # log = logger.Log()
+    print("INSIDE PROCESSREQUEST")
 
     sessionID=req.get('responseId')
 
@@ -54,46 +56,11 @@ def processRequest(req):
 
         fulfillmentText=""
 
-        def diseaseDetail(term):
-            diseases=[term]
-            ret=term+"\n"
-            for dis in diseases:
-                # search "disease wilipedia" on google
-                query = dis+' wikipedia'
-                # tld="co.in"
-                # ,stop=10,pause=0.5
-                for sr in search(query):
-                    # open wikipedia link
-                    match=re.search(r'wikipedia',sr)
-                    filled = 0
-                    if match:
-                        wiki = requests.get(sr,verify=False)
-                        soup = BeautifulSoup(wiki.content, 'html5lib')
-                        # Fetch HTML code for 'infobox'
-                        info_table = soup.find("table", {"class":"infobox"})
-                        if info_table is not None:
-                            # Preprocess contents of infobox
-                            for row in info_table.find_all("tr"):
-                                data=row.find("th",{"scope":"row"})
-                                if data is not None:
-                                    symptom=str(row.find("td"))
-                                    symptom = symptom.replace('.','')
-                                    symptom = symptom.replace(';',',')
-                                    symptom = symptom.replace('<b>','<b> \n')
-                                    symptom=re.sub(r'<a.*?>','',symptom) # Remove hyperlink
-                                    symptom=re.sub(r'</a>','',symptom) # Remove hyperlink
-                                    symptom=re.sub(r'<[^<]+?>',' ',symptom) # All the tags
-                                    symptom=re.sub(r'\[.*\]','',symptom) # Remove citation text                    
-                                    symptom=symptom.replace("&gt",">")
-                                    ret+=data.get_text()+" - "+symptom+"\n"
-                                    # print(data.get_text(),"-",symptom)
-                                    filled = 1
-                        if filled:
-                            break
-            return ret
+       
 
         for i in range(len(user_symptom)):
-            detail = user_symptom[i] + "\n" + diseaseDetail(user_symptom[i]) + "\n"
+            print("Inside for loop")
+            detail = user_symptom[i] + "\n" + "\n"
             fulfillmentText += detail
     
 
