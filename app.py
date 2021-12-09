3761f8f0058c63abdc9df24d9574ff7b7406d92a
Rashmiii1001 <63809222+Rashmiii1001@users.noreply.github.com>
2021-12-09 16:13:53 +0530

    elif

--- app.py
+++ app.py
@@ -113,45 +113,45 @@ def processRequest(req):
         fulfillmentText=""
 
         def diseaseDetail(term):
-            diseases=[term]
+            print(term)
             ret="  \n"
-            for dis in diseases:
-                # search "disease wilipedia" on google
-                query = dis+' wikipedia'
-                # tld="co.in"
-                # ,stop=10,pause=0.5
-                for sr in search(query):
-                    # open wikipedia link
-                    match=re.search(r'wikipedia',sr)
-                    filled = 0
-                    if match:
-                        wiki = requests.get(sr,verify=False)
-                        soup = BeautifulSoup(wiki.content, 'html5lib')
-                        # Fetch HTML code for 'infobox'
-                        info_table = soup.find("table", {"class":"infobox"})
-                        if info_table is not None:
-                            # Preprocess contents of infobox
-                            for row in info_table.find_all("tr"):
-                                data=row.find("th",{"scope":"row"})
-                                if data is not None:
-                                    symptom=str(row.find("td"))
-                                    symptom = symptom.replace('.','')
-                                    symptom = symptom.replace(';',',')
-                                    symptom = symptom.replace('<b>','<b> \n')
-                                    symptom=re.sub(r'<a.*?>','',symptom) # Remove hyperlink
-                                    symptom=re.sub(r'</a>','',symptom) # Remove hyperlink
-                                    symptom=re.sub(r'<[^<]+?>',' ',symptom) # All the tags
-                                    symptom=re.sub(r'\[.*\]','',symptom) # Remove citation text                    
-                                    symptom=symptom.replace("&gt",">")
-                                    ret+=data.get_text()+" - "+symptom+"\n"
-                                    # print(data.get_text(),"-",symptom)
-                                    filled = 1
-                        if filled:
-                            break
+            # for dis in diseases:
+            # search "disease wilipedia" on google
+            query = term +' wikipedia'
+            # tld="co.in"
+            # ,stop=10,pause=0.5
+            for sr in search(query):
+                # open wikipedia link
+                match=re.search(r'wikipedia',sr)
+                filled = 0
+                if match:
+                    wiki = requests.get(sr,verify=False)
+                    soup = BeautifulSoup(wiki.content, 'html5lib')
+                    # Fetch HTML code for 'infobox'
+                    info_table = soup.find("table", {"class":"infobox"})
+                    if info_table is not None:
+                        # Preprocess contents of infobox
+                        for row in info_table.find_all("tr"):
+                            data=row.find("th",{"scope":"row"})
+                            if data is not None:
+                                symptom=str(row.find("td"))
+                                symptom = symptom.replace('.','')
+                                symptom = symptom.replace(';',',')
+                                symptom = symptom.replace('<b>','<b> \n')
+                                symptom=re.sub(r'<a.*?>','',symptom) # Remove hyperlink
+                                symptom=re.sub(r'</a>','',symptom) # Remove hyperlink
+                                symptom=re.sub(r'<[^<]+?>',' ',symptom) # All the tags
+                                symptom=re.sub(r'\[.*\]','',symptom) # Remove citation text
+                                symptom=symptom.replace("&gt",">")
+                                ret+=data.get_text()+" - "+symptom+"\n"
+                                # print(data.get_text(),"-",symptom)
+                                filled = 1
+                    if filled:
+                        break
             return ret
 
         
-        detail =  "The information related to entered disease  \n"+ diseaseDetail(disease_info)
+        detail =  "Requested information -  \n"+ diseaseDetail(disease_info)
         fulfillmentText += detail
         print(fulfillmentText)
 
