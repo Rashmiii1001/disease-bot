3d727f7ead31cda9d23aea11e4b611a432438d64
hrithika2501 <71816317+hrithika2501@users.noreply.github.com>
2022-01-13 21:19:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,12 +15,10 @@ import pandas as pd
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 from sklearn.model_selection import train_test_split, cross_val_score
 from statistics import mean
-from nltk.corpus import wordnet 
+
 import requests
 from bs4 import BeautifulSoup
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import RegexpTokenizer
+
 from itertools import combinations
 from time import time
 from collections import Counter
@@ -29,7 +27,7 @@ from xgboost import XGBClassifier
 import math
 from sklearn.linear_model import LogisticRegression
 warnings.simplefilter("ignore")
-import nltk
+
 # nltk.download('all')
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
