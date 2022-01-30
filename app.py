4db1936e16fdda4c06ffd41393d5ffc9796362d7
hrithika2501 <71816317+hrithika2501@users.noreply.github.com>
2022-01-30 19:48:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ import numpy as np
 import pandas as pd
 import json
 import nltk
-# nltk.download('all')
+nltk.download('all')
 nltk.download('wordnet', quiet=True)
 import warnings
 warnings.filterwarnings("ignore")
