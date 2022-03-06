7b0f99a1bf98475c3f8183494bb42e576cdca094
Rashmiii1001 <63809222+Rashmiii1001@users.noreply.github.com>
2022-03-06 19:07:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -166,7 +166,12 @@ def processRequest(req):
                     if symp in user_sym.split():
                         count+=1
                 if count/len(data_sym_split)>0.5:
+                    if data_sym in found_symptoms:
+                      continue
+                else:
+                    print(data_sym)
                     found_symptoms.add(data_sym)
+                    
         found_symptoms = list(found_symptoms)
         print(found_symptoms)
 
