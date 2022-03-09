8b5b458584711da08cc90a3d777a6d35c23eace3
Rashmiii1001 <63809222+Rashmiii1001@users.noreply.github.com>
2022-03-09 18:52:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,8 +28,8 @@ import warnings
 warnings.filterwarnings("ignore")
 warnings.simplefilter("ignore")
 
-df = pd.read_csv("Testing.csv")
-df1 = pd.read_csv("Training.csv")
+df = pd.read_csv("Training.csv")
+df1 = pd.read_csv("Testing.csv")
 
 X = df1.iloc[:, 1:]
 Y = df1.iloc[:, 0:1]
